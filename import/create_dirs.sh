5df58e439aa69629481755393784ddd1fdee4cac
Jacek Śliwerski <rzyj@plusnet.pl>
2007-03-12 20:49:12 +0000

    Fix some stupid mistakes

--- import/create_dirs.sh
+++ import/create_dirs.sh
@@ -4,7 +4,7 @@ for i in `cat $1`; do
     if [ ! -d $i ]; then
 	mkdir $i
 	cd $i
-	for j in contact.html db.html favicon.ico google9242b257cfec1cea.html idea.css images index.html robots.txt template.html whatfor.html; do
+	for j in contact.html favicon.ico google9242b257cfec1cea.html idea.css images index.html robots.txt template.html whatfor.html; do
 	    ln -s ../en/$j .
 	done
 	sed -s "s/open-tran/$i.open-tran/" < ../en/search.xml > search.xml
