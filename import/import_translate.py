f5ef21d2b9abab1c7dfb69f52e70a02968e8c730
Jacek Śliwerski <rzyj@plusnet.pl>
2007-03-12 20:21:26 +0000

    New database

--- import/import_translate.py
+++ import/import_translate.py
@@ -19,14 +19,7 @@ from translate.storage import factory
 from phrase import Phrase
 from pysqlite2 import dbapi2 as sqlite
 
-import dircache, sys
-
-
-conn = sqlite.connect('../data/sixth.db')
-cursor = conn.cursor()
-
-cls = factory.getclass("kde.po")
-langs = ['de', 'es', 'fr', 'it', 'ja', 'nl', 'pl', 'pt', 'pt_BR', 'ru', 'sv', 'tr']
+import dircache, sys, os
 
 def store_phrase(lid, sentence, lang):
     global cursor
@@ -37,26 +30,31 @@ def store_phrase(lid, sentence, lang):
                    (lid, lang, sentence))
     cursor.execute(u"insert into canonical(locationid, lang, phrase) values (?, ?, ?)", \
                    (lid, lang, phrase.canonical ()))
+
     
-def store_phrases(phrases):
+def store_phrases(project, phrases):
     global cursor
     for source, ls in phrases.iteritems():
-        cursor.execute(u"insert into locations (project) values (?)", ("KDE " + fname[:-3],))
+        cursor.execute(u"insert into locations (project) values (?)", ("KDE " + project[:-3],))
         cursor.execute("select max (rowid) from locations")
         lid = cursor.fetchone()[0]
         store_phrase(lid, source, "C")
         for lang, target in ls.iteritems():
             store_phrase(lid, target, lang)
 
+
 def load_file(phrases, fname, lang):
     global cls
-    store = cls.parsefile('kde/' + lang + '/' + fname)
+    store = cls.parsefile(fname[:23] + lang + fname[25:])
+    mlang = lang.replace('@', '_').lower()
     for unit in store.units:
-        l = phrases.setdefault(str(unit.source), {})
-        l[lang] = str(unit.target)
+        if len(str(unit.target)) > 0:
+            l = phrases.setdefault(str(unit.source), {})
+            l[mlang] = str(unit.target)
     return len(store.units)
 
-def store_file(fname):
+
+def store_file(project, fname):
     global langs
     phrases = {}
     for lang in langs:
@@ -69,16 +67,46 @@ def store_file(fname):
             print "failed."
         sys.stdout.flush()
     print "  phrases: %d" % len(phrases)
-    store_phrases(phrases)
+    store_phrases(project, phrases)
 
-cursor.execute("drop index if exists loc_lang_idx")
 
-for fname in dircache.listdir('kde/de'):
-    print "Now importing %s..." % fname
+def log(text, nonline=False):
+    if nonline:
+        print text,
+    else:
+        print text
     sys.stdout.flush()
-    cursor = conn.cursor()
-    store_file(fname)
-    conn.commit()
 
+
+conn = sqlite.connect('../data/seventh.db')
+cursor = conn.cursor()
+
+cls = factory.getclass("kde.po")
+
+langs = ['af','ar','az','be','bg','bn','br','bs','ca','cs','cy','da','de','el','eo','es','et','eu','fa','fi','fo','fr','fy','ga','gl','he','hi','hr','hsb','hu','id','is','it','ja','kk','km','ko','ku','lb','lo','lt','lv','mi','mk','mn','ms','mt','nb','nds','ne','nl','nn','oc','pa','pl','pt','pt_BR','ro','ru','rw','se','sk','sl','sq','sr','sr@Latn','ss','sv','ta','tg','th','tr','tt','uk','uz','ven','vi','wa','xh','zh_CN','zh_HK','zh_TW','zu']
+
+log("Dropping index...", True)
+cursor.execute("drop index if exists loc_lang_idx")
+log("done.")
+
+for root, dirs, files in os.walk('/home/sliwers/kde-l10n/fr'):
+    for f in files:
+        if f.endswith('.po'):
+            log("Importing %s..." % f)
+            cursor = conn.cursor()
+            store_file(f, os.path.join(root, f))
+    if '.svn' in dirs:
+        dirs.remove('.svn')
+
+log("Creating index...", True)
 cursor.execute("create index loc_lang_idx on phrases (locationid, lang)")
+log("done.")
 conn.close()
+
+# for fname in dircache.listdir('kde/de'):
+#     print "Now importing %s..." % fname
+#     sys.stdout.flush()
+#     cursor = conn.cursor()
+#     store_file(fname)
+#     conn.commit()
+
