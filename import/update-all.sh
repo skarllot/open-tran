0a2c8b5e20380f11c49dd2cfd253c55d9a3eec2a
Jacek Śliwerski <rzyj@plusnet.pl>
2009-02-25 19:12:52 +0000

    svn up must be separate

--- import/update-all.sh
+++ import/update-all.sh
@@ -3,55 +3,9 @@
 . update.conf
 
 export LC_ALL=C
-export PYTHONPATH=$toolkit_path:$data_root/../lib
 export PATH=$PATH:$data_root/../import
-
-log="$log_dir/import.log"
-err="$log_dir/import.err"
-audit="$log_dir/audit.txt"
-status="$log_dir/status.txt"
-
-echo "initializing" > $status
 
 svn up $data_root/../lib > /dev/null 2>&1
 svn up $data_root/../import > /dev/null 2>&1
 
-echo "importing" >> $status
-rm -f $log $err $audit
-
-date > $log
-
-update () {
-    script="./update-$1.sh"
-    if test -z "$2"; then
-	proj=$1
-    else
-	proj=$2
-    fi
-    echo $proj >> $status
-    echo
-    echo "===== UPDATING $proj ====" >> $log
-    $script $data_root $2 $3 >> $log 2>> $err
-}
-
-update svn debian-installer svn://svn.d-i.alioth.debian.org/svn/d-i/trunk/packages/po
-update gnome
-update svn inkscape https://inkscape.svn.sourceforge.net/svnroot/inkscape/inkscape/trunk/po
-update kde
-update mozilla
-update oo
-update svn suse-i18n https://forgesvn1.novell.com/svn/suse-i18n/trunk
-update xfce
-
-
-rm -rf $data_root/*
-
-echo "processing" > $status
-
-import_step1.py $data_root >> $log 2>> $err
-audit_step1.py $data_root > $audit 2>> $err
-import_step2.py $data_root >> $log 2>> $err
-import_step3.py $data_root >> $log 2>> $err
-
-rm $status
-
+update-steps.sh
---
+++ import/update-steps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+. update.conf
+
+export LC_ALL=C
+export PYTHONPATH=$toolkit_path:$data_root/../lib
+export PATH=$PATH:$data_root/../import
+
+log="$log_dir/import.log"
+err="$log_dir/import.err"
+audit="$log_dir/audit.txt"
+status="$log_dir/status.txt"
+
+update () {
+    script="./update-$1.sh"
+    if test -z "$2"; then
+	proj=$1
+    else
+	proj=$2
+    fi
+    echo $proj >> $status
+    echo
+    echo "===== UPDATING $proj ====" >> $log
+    $script $data_root $2 $3 >> $log 2>> $err
+}
+
+
+rm -f $log $err $audit
+date > $log
+echo "importing" > $status
+
+update svn debian-installer svn://svn.d-i.alioth.debian.org/svn/d-i/trunk/packages/po
+update gnome
+update svn inkscape https://inkscape.svn.sourceforge.net/svnroot/inkscape/inkscape/trunk/po
+update kde
+update mozilla
+update oo
+update svn suse-i18n https://forgesvn1.novell.com/svn/suse-i18n/trunk
+update xfce
+
+rm -rf $data_root/../data/*
+
+echo "processing" >> $status
+
+date >> $log
+
+import_step1.py $data_root >> $log 2>> $err
+audit_step1.py $data_root > $audit 2>> $err
+import_step2.py $data_root >> $log 2>> $err
+import_step3.py $data_root >> $log 2>> $err
+
+date >> $log
+
+rm $status
