9c4966384d4cfcac0f1f24a0868e7a160b90a526
Jacek Śliwerski <rzyj@plusnet.pl>
2009-05-02 14:50:05 +0000

    Script for switching the imports

---
+++ import/update-switch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+if [[ -z "$IMPORTDIR" ]]; then
+    echo "IMPORTDIR is not defined.  It should point to the import directory."
+    exit 1
+fi
+
+PYTHONPATH="$IMPORTDIR/../lib"
+
+$IMPORTDIR/audit_step1.py $IMPORTDIR > /tmp/projects.html
+$IMPORTDIR/audit_step2.py $IMPORTDIR > /tmp/languages.html
+
+if file "$IMPORTDIR/../data" | grep "dataa"; then
+    olddir="$IMPORTDIR/../dataa"
+    newdir="$IMPORTDIR/../datab"
+else
+    olddir="$IMPORTDIR/../datab"
+    newdir="$IMPORTDIR/../dataa"
+fi
+
+rm "$IMPORTDIR/../data"
+ln -s "$newdir" "$IMPORTDIR/../data"
+mv /tmp/projects.html $IMPORTDIR/../server
+mv /tmp/languages.html $IMPORTDIR/../server
