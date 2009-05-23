541d2f2a848adde9aca8049d040e6cb4917c7400
Jacek Śliwerski <rzyj@plusnet.pl>
2009-05-23 10:47:56 +0000

    More date/time info

--- import/update-local.sh
+++ import/update-local.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=$local_dir/lib
 logout="$log_dir/import.log"
 errout="$log_dir/import.err"
 
-echo -n "Starting import: " >> $logout
+echo -n "########## Starting import: " >> $logout
 date >> $logout
 
 if file "$local_dir/data" | grep "dataa"; then
@@ -21,8 +21,14 @@ else
     newdir="$local_dir/dataa"
 fi
 
+echo -n "########## Downloading the database: " >> $logout
+date >> $logout
+
 wget -o /dev/null -O $newdir/ten.db http://ot.leonardof.org/data/ten.db
 rm -f $newdir/ten-*.db
+
+echo -n "########## Copying Mozilla and OO.o phrases: " >> $logout
+date >> $logout
 
 (cat <<EOF
 attach database '$local_dir/dataa/mo.db' as 'mo';
@@ -35,7 +41,13 @@ select * from mo.phrases;
 EOF
 ) | sqlite3 $newdir/ten.db
 
+echo -n "########## Moving phrases: " >> $logout
+date >> $logout
 $local_dir/import/import_step2.py $newdir >> $logout 2>> $errout
+
+echo -n "########## Moving words: " >> $logout
+date >> $logout
 $local_dir/import/import_step3.py $newdir >> $logout 2>> $errout
-echo -n "Finished " >> $logout
+
+echo -n "########## Finished " >> $logout
 date >> $logout
