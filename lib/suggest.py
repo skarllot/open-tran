9ae6219b4451638747561668b56a8ec486dcea40
Jacek Śliwerski <rzyj@plusnet.pl>
2010-01-31 08:55:43 +0000

    Switch from pysqlite2 to native sqlite3 dbapi2

--- lib/suggest.py
+++ lib/suggest.py
@@ -15,7 +15,7 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
 
-from pysqlite2 import dbapi2 as sqlite
+from sqlite3 import dbapi2 as sqlite
 from xml.sax.saxutils import escape, quoteattr
 from phrase import Phrase
 import sys, os
