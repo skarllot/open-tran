c09e4efa8aa183171fd56c415868cb873d24d90f
Jacek Śliwerski <rzyj@plusnet.pl>
2010-02-28 18:18:12 +0000

    Fixed the starting script

--- server/start.sh
+++ server/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SERVERDIR=`dirname $0`
-export PYTHONPATH=$SERVERDIR/../lib
+cd `dirname $0`
+export PYTHONPATH=../lib
 
-if [[ -f $SERVERDIR/server.pid ]]; then
-    RUNNING=`cat $SERVERDIR/server.pid`
+if [[ -f server.pid ]]; then
+    RUNNING=`cat server.pid`
     if kill -0 $RUNNING 2> /dev/null; then
 	exit 0
     fi
 fi
 
-nohup authbind python $SERVERDIR/server.py > /tmp/ot.out 2> /tmp/ot.err &
+nohup authbind python server.py > /tmp/ot.out 2> /tmp/ot.err &
