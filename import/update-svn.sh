0b81b6fb5c231830693bdbf56ebc1b770f91427e
Jacek Śliwerski <rzyj@plusnet.pl>
2010-04-23 08:53:42 +0000

    Checkout change

--- import/update-svn.sh
+++ import/update-svn.sh
@@ -7,7 +7,7 @@ proj_name="$2"
 url="$3"
 svn_root="$data_root/$proj_name"
 
-if test -d $svn_root; then
+if test -d "$svn_root/.svn"; then
     cd $svn_root
 
     echo -n "cleanup $proj_name..."
@@ -18,8 +18,9 @@ if test -d $svn_root; then
     svn up > /dev/null
     echo "done."
 else
+    rm -rf $svn_root
     echo -n "co $proj_name..."
     cd $data_root
-    echo "p" | svn co $url $svn_root > /dev/null
+    echo "p" | svn co $url $svn_root 2>&1
     echo "done."
 fi
